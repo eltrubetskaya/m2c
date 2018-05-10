9c671b4f35ee2d6ac2f5a061d2da25b098bd436e
Elena Trubetskaia <olena.trubetska@ekreative.com>
2018-05-10 14:51:30 +0300

    create no route handler

--- app/code/Training/ModuleRouting/Controller/NoRouteHandler.php
+++ app/code/Training/ModuleRouting/Controller/NoRouteHandler.php
@@ -2,6 +2,29 @@
 
 namespace Training\ModuleRouting\Controller;
 
-class NoRouteHandler
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\Router\NoRouteHandlerInterface;
+
+class NoRouteHandler implements NoRouteHandlerInterface
 {
+    /**
+     * Check and process no route request
+     *
+     * @param RequestInterface $request
+     * @return bool
+     */
+    public function process(RequestInterface $request)
+    {
+        $moduleName = 'cms';
+        $controllerName = 'index';
+        $actionName = 'index';
+
+        $request
+            ->setModuleName($moduleName)
+            ->setControllerName($controllerName)
+            ->setActionName($actionName)
+        ;
+
+        return true;
+    }
 }
